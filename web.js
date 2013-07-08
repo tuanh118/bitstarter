0fd2f868509fdc8695d9c7c2c0f7c55a1792075d
Tuan Anh Pham <tuanh118@gmail.com>
2013-07-08 09:52:29 +0000

    mmmmm

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var index = fs.readFileSync('/index.html');
+var index = fs.readFileSync('index.html');
 var buffer = new Buffer(index);
 
 app.get('/', function(request, response) {
