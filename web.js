51f1162d2fabb9c4e422d938f6345d7c5dabe260
Tuan Anh Pham <tuanh118@gmail.com>
2013-07-08 08:59:48 +0000

    Web.js now reads index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var index = fs.readFileSync('index.html');
+var buffer = new Buffer(index);
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+    response.send(index);
 });
 
 var port = process.env.PORT || 5000;
